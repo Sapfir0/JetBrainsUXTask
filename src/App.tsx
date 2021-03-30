4e2dc9b2af60497f9f10aaaee9f19d5894ef8d30
Sapfir0 <sapfir999999@yandex.ru>
2021-03-30 16:42:25 +0300

    Added typings

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { VirtualizedTable } from './Components/VirtualizedTable/VirtualizedTable';
+import { Columns } from './typings';
+
 
 function App() {
+    const columns: Columns = [
+        {name: 'id'},
+        {name: 'data1'},
+        {name: 'data2'},
+        {name: 'data3'},
+        {name: 'data4'},
+    ]
+
     return (
         <div className="App">
-            <VirtualizedTable />
+            <VirtualizedTable columns={columns} />
         </div>
     );
 }
--- src/Components/VirtualizedTable/VirtualizedTable.tsx
+++ src/Components/VirtualizedTable/VirtualizedTable.tsx
@@ -1,22 +1,27 @@
 import { useMemo, useState } from 'react';
 import { FixedSizeGrid } from 'react-window';
 import './VirtualizedTable.css';
-import {readFileSync} from 'fs';
 import tableData from "../../data.json"
+import { Columns } from '../../typings';
 
 
 const Cell = ({ columnIndex, data, rowIndex, style }: any) => {
     const { hoveredRowIndex, setHoveredRowIndex } = data;
     const className = hoveredRowIndex === rowIndex ? 'CellHovered' : 'Cell';
-
+    console.log(data)
     return (
         <div className={className} onMouseEnter={() => setHoveredRowIndex(rowIndex)} style={style}>
-            {tableData[rowIndex].id} {columnIndex}
+            {rowIndex} {columnIndex}
+            {/* {tableData[rowIndex][`data${columnIndex}` as any] as any} */}
         </div>
     );
 };
 
-export const VirtualizedTable = () => {
+type VirtualizedTableProps = {
+    columns: Columns
+}
+
+export const VirtualizedTable = (props: VirtualizedTableProps) => {
     const [hoveredRowIndex, setHoveredRowIndex] = useState(null);
     const itemData = useMemo(
         () => ({
@@ -35,6 +40,7 @@ export const VirtualizedTable = () => {
                 height={800}
                 itemData={itemData}
                 rowCount={tableData.length}
+                // rowCount={50000}
                 width={1100}
             >
                 {Cell}
